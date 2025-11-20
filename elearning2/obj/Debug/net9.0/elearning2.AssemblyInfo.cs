f6b275075fe483e8628420d0fc121a4aff15cd0a
CalebBav234 <calebmbaving@gmail.com>
2025-11-20 08:57:14 -0400

    Feat: created Login  Response

--- elearning2/obj/Debug/net9.0/elearning2.AssemblyInfo.cs
+++ elearning2/obj/Debug/net9.0/elearning2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3014b036a27d53a6d042838dc16cc8d2598803e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ed184265eb81296920ae68f311c121ee8de847")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

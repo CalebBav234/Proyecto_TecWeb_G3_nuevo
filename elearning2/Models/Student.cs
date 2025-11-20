53202658d06ea12a860e1cea2a93547ab2408c98
CalebBav234 <calebmbaving@gmail.com>
2025-11-20 08:39:38 -0400

    Feat: created Student.cs

---
+++ elearning2/Models/Student.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace elearning2.Models;
+
+public class Student
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string FullName { get; set; } = null!;
+    public string? Bio { get; set; }
+    public string? AvatarUrl { get; set; }
+
+    public User User { get; set; } = null!;
+}

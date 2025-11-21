cdbfb3405f9c3391dc32417565b11052662ad023
CalebBav234 <calebmbaving@gmail.com>
2025-11-20 20:29:57 -0400

    feat: updated docker compose

--- elearning2/obj/Debug/net9.0/elearning2.AssemblyInfo.cs
+++ elearning2/obj/Debug/net9.0/elearning2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02738c9b8cd524e288434599c06d60944c0c05f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b9879ef047ae3c5729e9c6d78e44791321e5d91")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

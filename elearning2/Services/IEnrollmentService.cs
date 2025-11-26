691df4822b519f5a9b14a6ac1b4627b9ae7150eb
Soria <rene.soria@ucb.edu.bo>
2025-11-26 15:59:49 -0400

    fix:  Add IEnrollmentService

--- elearning2/Services/IEnrollmentService.cs
+++ elearning2/Services/IEnrollmentService.cs
@@ -1,6 +1,17 @@
-﻿namespace elearning2.Services
+﻿using elearning2.Models;
+using elearning2.Models.DTOS;
+
+namespace elearning2.Services
 {
     public interface IEnrollmentService
     {
+        Task<IEnumerable<Enrollment>> GetAll();
+        Task<Enrollment?> GetOne(Guid id);
+        Task<IEnumerable<Enrollment>> GetByStudentId(Guid studentId);
+        Task<IEnumerable<Enrollment>> GetByCourseId(Guid courseId);
+
+        Task<Enrollment> CreateEnrollment(CreateEnrollmentDto dto);
+        Task<Enrollment?> UpdateEnrollment(UpdateEnrollmentDto dto, Guid id);
+        Task<bool> DeleteEnrollment(Guid id);
     }
 }

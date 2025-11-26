549a005a6207444fb568dce45a57165beb380afc
Soria <rene.soria@ucb.edu.bo>
2025-11-26 15:41:30 -0400

    feat: Add IEnrollmentRepository

---
+++ elearning2/Repositories/IEnrollmentRepository.cs
@@ -0,0 +1,17 @@
+﻿using elearning2.Models;
+
+namespace elearning2.Repositories
+{
+    public interface IEnrollmentRepository
+    {
+        Task<IEnumerable<Enrollment>> GetAll();
+        Task<Enrollment?> GetOne(Guid id);
+        Task<IEnumerable<Enrollment>> GetByStudentId(Guid studentId);
+        Task<IEnumerable<Enrollment>> GetByCourseId(Guid courseId);
+        Task<Enrollment?> GetByStudentAndCourse(Guid studentId, Guid courseId);
+
+        Task Add(Enrollment enrollment);
+        Task Update(Enrollment enrollment);
+        Task Delete(Enrollment enrollment);
+    }
+}

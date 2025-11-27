fdafce0bd692ffe647e5dd3d60778cf2cbc79754
Flora <flora.ortiz@ucb.edu.bo>
2025-11-26 23:27:25 -0400

    feat: implement repository interface

---
+++ elearning2/Repositories/CourseRepository.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using elearning2.Data;
+using elearning2.Models;
+
+namespace elearning2.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        public Task AddCourse(Course course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCourse(Course course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Course>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Course?> GetById(Guid courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Course>> GetBYTeacher(Guid teacherId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Course?> GetByTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCourse(Course course)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

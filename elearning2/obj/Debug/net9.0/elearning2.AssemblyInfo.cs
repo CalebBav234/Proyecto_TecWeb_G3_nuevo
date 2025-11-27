ff323b44152a49c790efbf41efa9d71cb9ea567d
CalebBav234 <calebmbaving@gmail.com>
2025-11-27 08:01:50 -0400

    fix port configuration

--- elearning2/obj/Debug/net9.0/elearning2.AssemblyInfo.cs
+++ elearning2/obj/Debug/net9.0/elearning2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4992d16805d7a57c17e34af3fa8ba22463f84207")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc66258afc71b451de22a5dbf38e532ccc9b3273")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
